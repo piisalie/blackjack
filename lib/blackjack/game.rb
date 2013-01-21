7cb258a1336b373cf91a5c99bdf64174fc0a60aa
Paul Dawson <excidere@gmail.com>
2013-01-21 10:16:08 -0600

    Started building the game logic.

--- lib/blackjack/game.rb
+++ lib/blackjack/game.rb
@@ -1,12 +1,56 @@
 module Blackjack
   class Game
-    attr_reader :dealer, :player
     
     def initialize
       @shoe   = Shoe.new
       @dealer = Hand.new(@shoe)
       @player = Hand.new(@shoe)
     end
-    
+
+    def hit(player)
+      player.hit
+    end
+
+    def show_hands
+      puts "Dealer's hand: #{@dealer.hand[1]}"
+      puts "Your Hand:     #{@player.hand}"
+    end
+
+    def execute(action)
+      if action == "h" || action == "hit"
+        @player.hit
+        show_hands
+        get_player_input
+      elsif
+        action == "s" || action == "stand"
+        play_dealer
+      else
+        puts "\n --Please choose a valid option.--\n\n"
+        get_player_input
+      end
+    end
+
+    def show_scores
+      puts "Dealer's score: #{@dealer.score}"
+      puts "Your score:     #{@player.score}"
+    end
+
+    def get_player_input
+      puts "(h)it or (s)tand?"
+      action = gets.chomp
+      execute(action)
+    end
+
+    def play_dealer
+      while @dealer.score <= 17
+        @dealer.hit
+      end
+    end
+
+    def play
+      show_hands
+      get_player_input
+      show_scores
+    end
   end
 end
