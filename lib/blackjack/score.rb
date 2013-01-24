b3b9804436e3316a04ecbfe17a86e4fd61134ad6
Paul Dawson <excidere@gmail.com>
2013-01-23 23:16:08 -0600

    Fixed (another) ace scoring error.

--- lib/blackjack/score.rb
+++ lib/blackjack/score.rb
@@ -12,21 +12,29 @@ module Blackjack
       @hand.each do |card|
         @total = @total + score_card(card)
       end
+      @hand.each do |card|
+        @total = @total + score_ace(card)
+      end
       @total
     end
     
     def score_card(card)
       if card == "K" || card == "Q" || card == "J"
         10
-      elsif
-        card == "A"
-        if  @total <= 10
-          11
-        else
-          1
-        end
+      elsif card == "A"
+        0
       else
         card
+      end
+    end
+
+    def score_ace(card)
+      if card == "A" and @total + 11 <= 21
+        11
+      elsif card == "A"
+        1
+      else
+        0
       end
     end
 
